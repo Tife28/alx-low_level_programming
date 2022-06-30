cfdaade9a7d891ff4cc6784e15cf732c0258899d
Tife <61167252+Tife28@users.noreply.github.com>
2022-06-30 23:46:48 +0100

    Update 101-quote.c

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,13 +1,12 @@
 #include <stdio.h>
 /**
- * main - prints exactly and that piece of art is useful" - Dora Korpar, 2015-10-19
- *
+ * main - prints exactly and that piece of art 
+ * is useful" - Dora Korpar, 2015-10-19
  * Return: Always 1 (Success)
  */
 int main(void)
 {
-    char str1[] ="and that piece of art is useful\" - Dora Korpar, 2015-10-19\n";
-
+    char str1[] = "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n";
     fwrite(2, str1, 59);
     return (1);
 }
