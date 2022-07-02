033e5234c57fb8fefb8c3c7a1bfa4c426425293d
Tife28 <boludayo@ekunstech.com>
2022-07-02 13:25:54 -0700

    Print alphabet

--- 0x01-variables_if_else_while/0-positive_or_negative.c
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -3,10 +3,10 @@
 #include <stdio.h>
 
 /**
- *  * main - Determines if a number is positive, negative or zero.
- *   *
- *    * Return: Always 0 (Success)
- *     */
+ * main - Determines if a number is positive, negative or zero.
+ *
+ * Return: Always 0 (Success)
+ */
 int main(void)
 {
 int n;
@@ -18,7 +18,7 @@ if (n > 0)
 printf("%d is positive\n", n);
 }
 else if (n == 0)
-}
+{
 printf("%d is zero\n", n);
 }
 else
--- 0x01-variables_if_else_while/1-last_digit.c
+++ 0x01-variables_if_else_while/1-last_digit.c
@@ -1,14 +1,12 @@
-
-
 #include <stdlib.h>
 #include <time.h>
 #include <stdio.h>
 
 /**
- *  * main - Determines either greater than 5, is less than 6, or is 0
- *   *
- *    * Return: Always 0 (Success)
- *     */
+ * main - Determines either greater than 5, is less than 6, or is 0
+ *
+ * Return: Always 0 (Success)
+ */
 int main(void)
 {
 int n, l;
