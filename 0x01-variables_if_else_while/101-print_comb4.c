a2c5acf7608cac101b6ce030c71d07e1f4eb69a9
Tife28 <boludayo@ekunstech.com>
2022-07-03 12:26:36 -0700

    Print

--- 0x01-variables_if_else_while/101-print_comb4.c
+++ 0x01-variables_if_else_while/101-print_comb4.c
@@ -28,7 +28,7 @@ putchar(i);
 putchar(g);
 if (i == 56 && e == 55 && g == 57)
 {
-  break;
+break;
 }
 putchar(',');
 putchar(' ');
--- 0x01-variables_if_else_while/9-print_comb.c
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -7,13 +7,13 @@
  */
 int main(void)
 {
-int i;
+int 1;
 
 for (i = 48; i < 58; i++)
 {
 putchar(i);
 if (i != 57)
-{				
+{
 putchar(',');
 putchar(' ');
 }
