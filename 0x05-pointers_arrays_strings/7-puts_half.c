d9cbbdc9c72c969b3928610338289910a7f8e6c8
Tife <61167252+Tife28@users.noreply.github.com>
2022-07-12 20:37:05 +0100

    Update 7-puts_half.c

--- 0x05-pointers_arrays_strings/7-puts_half.c
+++ 0x05-pointers_arrays_strings/7-puts_half.c
@@ -1,17 +1,17 @@
-#include "holberton.h"
+#include "main.h"
 
 /**
- *  * puts_half - prints half of a string.
- *   * @str: input string.
- *    * Return: no return.
- *     */
+ * puts_half - prints half of a string.
+ * @str: input string.
+ * Return: no return.
+ */
 void puts_half(char *str)
 {
-		int count = 0, i;
+int count = 0, i;
 
-			while (count >= 0)
-					{
-							if (str[count] == '\0')
+while (count >= 0)
+{
+if (str[count] == '\0')
 break;
 count++;
 }
