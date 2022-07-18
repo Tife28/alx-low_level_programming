fe7e3536b2c9e3b5adac04a48107006696624b64
Tife <61167252+Tife28@users.noreply.github.com>
2022-07-18 21:57:53 +0100

    It's a string

--- 0x07-pointers_arrays_strings/5-strstr.c
+++ 0x07-pointers_arrays_strings/5-strstr.c
@@ -1,11 +1,10 @@
 #include "main.h"
 
 /**
- * strstr - finds the first occurrence of the substring.
+ * _strstr - finds the first occurrence of the substring.
  * needle in the string haystack.
  * @haystack: entire string.
  * @needle: substring.
- * _strstr: string
  * Return: pointer to the beginning of located substring or
  * NULL if the substring is not found.
  */
